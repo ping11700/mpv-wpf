35a2cdc57af4d4e75a1faf0276cc93aa32c910d5
至平 吴 <wwww.ping11700@live.com>
2025-01-09 18:19:02 +0800

    remove host

--- mpv-wpf/App.xaml.cs
+++ mpv-wpf/App.xaml.cs
@@ -5,46 +5,17 @@
 /// </summary>
 public partial class App : Application
 {
-    public T? GetRequiredService<T>() where T : class => _host?.Services.GetRequiredService<T>();
-
-    private IHost? _host;
+  
 
     protected override async void OnStartup(StartupEventArgs e)
     {
         base.OnStartup(e);
 
-        _host = Host.CreateDefaultBuilder(e.Args).ConfigureHostConfiguration(configHost =>
-                {
-                    configHost.SetBasePath(AppContext.BaseDirectory);
-                    configHost.AddEnvironmentVariables(prefix: "PREFIX_");
-                    configHost.AddCommandLine(e.Args);
-                })
-                .UseContentRoot(AppContext.BaseDirectory)
-                .ConfigureAppConfiguration(appConfig =>
-                {
-                    //var configPath = SavePaths.ConfigDirectory;
-                    // 如果不存在 AccountConfig 则尝试加载 AppConfig
-                    //appConfig.AddJsonFile(Path.Combine(configPath, "AppConfig"), optional: true, reloadOnChange: true);
-                })
-                .ConfigureServices(ConfigureServices)
-                .ConfigureLogging(logging =>
-                {
-                    logging.ClearProviders();
-                    //logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Warning);
-                    //logging.AddConsole();
-                    //logging.AddDebug();
-                    //logging.AddNLog();
-                })
-                .Build();
-
-        await _host.StartAsync();
+         
      }
 
 
-    private static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
-    {
-        
-    }
+ 
 
 
     /// <summary>
@@ -56,13 +27,7 @@ public partial class App : Application
 
         try
         {
-            if (app._host != null)
-            {
-                app._host.StopAsync();
-                app._host.Dispose();
-                app._host = null;
-            }
-
+             
             Application.Current.Shutdown();
         }
         finally
